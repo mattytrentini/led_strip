ccde3265bb1acb2a00f95f77b10576fdf95351c9
Matt Trentini <matt.trentini@gmail.com>
2017-05-29 18:08:53 +1000

    Very hacky workaround to ignore the rotary encoder except when LED's are fully on (ie not off or fading).
    Can definitely reduce the number of variables...

--- main.py
+++ main.py
@@ -29,28 +29,43 @@ def main(enc):
     rate = 20
     oldval = 0
     on = False
-    
+
+    val = enc.value
+    last_set_to = val
     try:
         while True:
-            val = enc.value
             if not on and pressed:
-                fade(start=0, end=val+1)
+                
+                enc._value = last_set_to
+                print('Turning on. last_set_to = {}, val = {}, oldval = {}, enc.val = {}'.format(last_set_to, val, oldval, enc.value))
+                
+                # Could debounce here. Wait here for x milliseconds. Check button again.
+                fade(start=0, end=last_set_to+1)
+                sleep_ms(100)
                 pressed = False
                 on = True
             elif on and pressed:
+                print('Turning off at {}'.format(val))
+                last_set_to = val
+                #enc._value = oldval
                 fade(start=val, end=-1, step=-1)
-                pressed = False 
+                sleep_ms(100)
+                pressed = False
                 on = False
 
-            if on and oldval != val:
-                print(val)
-                pwm.duty(val)
-                oldval = val
+            if on and not pressed:
+                val = enc.value
+                if oldval != val:
+                    print(val)
+                    pwm.duty(val)
+                    oldval = val
 
-            enc.cur_accel = max(0, enc.cur_accel - enc.accel)
+                enc.cur_accel = max(0, enc.cur_accel - enc.accel)
             sleep_ms(1000 // rate)
     except:
         enc.close()
 
-enc = Encoder(pin_clk=13, pin_dt=12, pin_mode=Pin.PULL_UP, min_val=1, max_val=1020, clicks=1, accel=5)
+enc = Encoder(pin_clk=13, pin_dt=12, pin_mode=Pin.PULL_UP,
+              min_val=1, max_val=1020, clicks=1, accel=5)
+enc._value = 1020
 main(enc)
