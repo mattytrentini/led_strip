67879340c9e7a458cc2359f527115440d5874708
Matt Trentini <matt.trentini@gmail.com>
2017-08-13 11:45:44 +1000

    Updated to use asyncio. Gideon contributed most of this effort! It needs to be tidied up but it's a great start.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import uasyncio as asyncio
 from machine import Pin, PWM
 from encoder import Encoder
 from time import sleep_us, sleep_ms
 
-pwm = PWM(Pin(4))
+pwm = PWM(Pin(15))
 button = Pin(14, Pin.IN, Pin.PULL_UP)
 pressed = False
 
@@ -12,11 +13,59 @@ def button_pressed(_):
 
 button.irq(trigger=Pin.IRQ_FALLING, handler=button_pressed)
 
-def fade(start=0, end=1020, step=1, duration_ms=1000):
-    sleep_interval_us = duration_ms * 1000 // abs(end-start)
-    for i in range(start, end, step):
-        pwm.duty(i)
-        sleep_us(sleep_interval_us)
+queue = []
+val = 0
+
+async def switch_loop():
+    global pressed
+    global queue
+    global val
+    on = False
+    while True:
+        if pressed:
+            asyncio.sleep(0.100) # Debounce duration
+            if on: # Turning off, fading out
+                print('Turning off at {}'.format(val))
+                queue.append([val, -1])
+            else: # Turning on, fading in
+                #print('Turning on. val = {}, oldval = {}, enc.val = {}'.format(val, oldval, enc.value))
+                queue.append([0, val+1])
+                enc._value = val
+            pressed = False
+            on = not on
+        await asyncio.sleep(0.100)
+
+async def encoder_loop(enc):
+    global queue
+    global val
+    oldval = 0
+    val = enc.value
+    while True:
+        val = enc.value
+        enc.cur_accel = max(0, enc.cur_accel - enc.accel)
+        if oldval != val:
+            print(val)
+            queue.append([oldval, val])
+            oldval = val
+        await asyncio.sleep(0.100)
+
+async def fader_loop():
+    global queue
+
+    async def fade(old_value, new_value):
+        for value in range(old_value, new_value, 1 if new_value >= old_value else -1):
+            pwm.duty(value)
+            #print('pwm.duty({0})'.format(value))
+            await asyncio.sleep(0.001)
+            if queue:
+                queue[0][0] = value
+                break
+
+    while True:
+        if queue:
+            old_value, new_value = queue.pop(0)
+            await fade(old_value, new_value)
+        await asyncio.sleep(0.100)
 
 def main(enc):
     global pressed
@@ -26,31 +75,14 @@ def main(enc):
     on = False
 
     val = enc.value
-    try:
-        while True:
-            if on and not pressed: # On, normal state
-                val = enc.value
-                enc.cur_accel = max(0, enc.cur_accel - enc.accel)
-                if oldval != val:
-                    print(val)
-                    pwm.duty(val)
-                    oldval = val
+    loop = asyncio.get_event_loop()
+    loop.create_task(switch_loop())
+    loop.create_task(encoder_loop(enc))
+    #try:
+    loop.run_until_complete(fader_loop())
+    #except:
+    #    enc.close()
 
-            if pressed:
-                sleep_ms(100) # Debounce duration
-                if on: # Turning off, fading out
-                    print('Turning off at {}'.format(val))
-                    fade(val, -1, -1)
-                else: # Turning on, fading in
-                    print('Turning on. val = {}, oldval = {}, enc.val = {}'.format(val, oldval, enc.value))
-                    fade(0, val + 1)
-                    enc._value = val
-                pressed = False
-                on = not on
-
-            sleep_ms(1000 // rate)
-    except:
-        enc.close()
 
 enc = Encoder(pin_clk=13, pin_dt=12, pin_mode=Pin.PULL_UP,
               min_val=40, max_val=1020, clicks=1, accel=5)
