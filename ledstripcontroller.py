300f061256cf1db7b8f43cde1c2549901c2ec5dd
Oliver Robson <ollsome@hotmail.com>
2018-04-28 19:55:30 +1000

    Refactoring to bring button code out of LedStripController and implementing PIR

--- ledstripcontroller.py
+++ ledstripcontroller.py
@@ -2,16 +2,14 @@ import uasyncio as asyncio
 
 class LedStripController:
 
-    def __init__(self, enc, button_pin, fader_pins):
+    def __init__(self, enc, fader_pins):
         ''' enc should be an instance of Encoder.
-            button_pin is expected to be an instance of machine.Pin.
             fader_pins is expected to be an iterable of machine.PWM's. 
         '''
         self.enc = enc
         self.is_on = False
         self.enc_cur_val = 511
         self.fader_target_val = 0
-        self.button_pin = button_pin
         self.fader_pins = fader_pins
         self.button_pressed = False
         for fader in self.fader_pins:
@@ -20,28 +18,29 @@ class LedStripController:
     def toggle_led_state(self):
         self.button_pressed = True
         print('Button pressed!')
+    
+    def led_state_off(self):
+        if not self.is_on:
+            toggle_led_state()
+    
+    def led_state_on(self):
+        if not self.is_off:
+            toggle_led_state()
 
     async def switch_loop(self, enc):
-
-        self.button_pressed = False
-
+        
         while True:
             if self.button_pressed:
-                # TODO should removedebouncing; should be handled outside of this class.
-                # Shouldn't care that it's a button that triggers the change...
-                asyncio.sleep_ms(100) # Debounce duration
-                # Only alter state if button still pressed (not a transient event)
-                if not self.button_pin.value():
-                    if self.is_on:
-                        # Turning off, fading out
-                        print('Turning off at {}'.format(self.enc_cur_val))
-                        self.fader_target_val = 0
-                    else:
-                        # Turning on, fading in
-                        print('Turning on at {}'.format(self.enc_cur_val))
-                        self.fader_target_val = enc._value = self.enc_cur_val
-                    self.button_pressed = False
-                    self.is_on = not self.is_on
+                if self.is_on:
+                    # Turning off, fading out
+                    print('Turning off at {}'.format(self.enc_cur_val))
+                    self.fader_target_val = 0
+                else:
+                    # Turning on, fading in
+                    print('Turning on at {}'.format(self.enc_cur_val))
+                    self.fader_target_val = enc._value = self.enc_cur_val
+                self.is_on = not self.is_on
+                self.button_pressed = False
             await asyncio.sleep_ms(100)
 
     async def encoder_loop(self, enc):
--- main.py
+++ main.py
@@ -1,34 +1,72 @@
-from machine import Pin, PWM
+from machine import Pin, PWM, Timer
 from encoder import Encoder
 from ledstripcontroller import LedStripController
-from uasyncio import get_event_loop
+import uasyncio as asyncio
 from sys import platform
+
+# Declarations
+debounce_check = False
+controller = None
+countdown_timer = Timer(-1)
+
+def button_debounce(pin):
+    global debounce_check, controller
+    
+    if not debounce_check:
+        debounce_check = True
+        asyncio.sleep_ms(100) # Debounce duration
+        # Only alter state if button still pressed (not a transient event)
+        if not pin.value():
+            controller.toggle_led_state()
+        debounce_check = False
+
+def pir_handler(pin):
+    global controller, countdown_timer
+    
+    if pin.value():
+        countdown_timer.deinit()
+        print("Motion detected!")
+        controller.led_state_on()
+    else:
+        print("Motion ceased...")
+        countdown_timer.init(period=300000, mode=Timer.ONE_SHOT, callback=controller.led_state_off)
+        
 
 def main():
     dev_type = platform
     if dev_type == 'esp8266': # WEMOS D1 Mini (or similar)
-        ENC_PINS = {'clk':4, 'dt':5, 'button':2}
+        ENC_PINS = {'clk':4, 'dt':5}
         FADER_PINS = (12, 13, 14)
         FADER_FREQ = 1000
+        BUTTON_PIN = 2
+        PIR_PIN = 0
     elif dev_type == 'esp32': # TTGO
-        ENC_PINS = {'clk':21, 'dt':22, 'button':16}
+        ENC_PINS = {'clk':21, 'dt':22}
         FADER_PINS = (19, 23, 18)
         FADER_FREQ = 50000
+        BUTTON_PIN = 16
+        PIR_PIN = 17
     else: # Defaults
         ENC_PINS = {'clk':None, 'dt':None, 'button':None}
         FADER_PINS = (None, None, None)
         FADER_FREQ = None
+        BUTTON_PIN = None
+        PIR_PIN = None
     
     enc = Encoder(pin_clk=ENC_PINS['clk'], pin_dt=ENC_PINS['dt'], pin_mode=Pin.PULL_UP,
                   min_val=40, max_val=1020, clicks=1, accel=5)
     enc._value = 0
 
     fader_pins = [PWM(Pin(pin_num), freq=FADER_FREQ, duty=0) for pin_num in FADER_PINS]
-    button = Pin(ENC_PINS['button'], Pin.IN, Pin.PULL_UP)
-    controller = LedStripController(enc, button_pin=button, fader_pins=fader_pins)
+    controller = LedStripController(enc, fader_pins=fader_pins)
+    
+    button = Pin(BUTTON_PIN, Pin.IN, Pin.PULL_UP)
     button.irq(trigger=Pin.IRQ_FALLING, handler=lambda _: controller.toggle_led_state())
+    
+    pir = Pin(PIR_PIN, Pin.IN)
+    pir.irq(trigger=(Pin.IRQ_FALLING | Pin.IRQ_RISING), handler=pir_handler)
 
-    loop = get_event_loop()
+    loop = asyncio.get_event_loop()
     loop.create_task(controller.switch_loop(controller.enc))
     loop.create_task(controller.encoder_loop(controller.enc))
     loop.create_task(controller.fader_loop())
